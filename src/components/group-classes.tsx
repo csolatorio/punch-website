20c2cc54b1906c47d36149b45e51741c8d78d28c
csolatorio <solatoriocarylethel@gmail.com>
2025-03-28 15:27:40 +0800

    updated homepage

--- src/components/group-classes.tsx
+++ src/components/group-classes.tsx
@@ -20,8 +20,9 @@ const ComingSoon: React.FC = () => {
           <h1 className="text-4xl sm:text-5xl font-bold text-white mb-4">
             🚧 Coming Soon 🚧
           </h1>
-          <p className="text-gray-300 text-lg sm:text-xl mb-6">
-            We're cooking up something amazing! Stay tuned for updates.
+          <p className="text-gray-300 text-lg sm:text-xl mt-10 mb-6">
+            Punch and empower your team through the transformative art of
+            boxing.
           </p>
           <div className="flex justify-center">
             {/* <button className="px-6 py-2 bg-indigo-600 hover:bg-indigo-500 text-white rounded-2xl shadow-lg transition duration-300">
