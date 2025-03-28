1d0a6deb604d4da9e428a413150446aca2444f36
csolatorio <solatoriocarylethel@gmail.com>
2025-03-28 12:40:56 +0800

    updated homepage

--- src/components/hero-page.tsx
+++ src/components/hero-page.tsx
@@ -9,12 +9,18 @@ const HeroPage = () => {
         className="relative w-full min-h-[75vh] flex items-center justify-start bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: `url(${heroBanner})` }}
       >
-        <div className="absolute inset-0 bg-white opacity-50"></div>
-        <div className="relative max-w-6xl text-black text-left p-8 ml-12 lg:ml-10 md:ml-6 sm:ml-4 sm:p-4">
-          <h1 className="text-6xl lg:text-8xl md:text-3xl sm:text-2xl font-semibold mb-8 leading-tight">
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative max-w-6xl text-white text-left p-8 ml-12 lg:ml-10 md:ml-6 sm:ml-4 sm:p-4">
+          <h1
+            className="text-6xl lg:text-8xl md:text-3xl sm:text-2xl font-semibold mb-8 leading-tight"
+            style={{ color: "white" }}
+          >
             A Boxing Workout That Hits Different
           </h1>
-          <p className="text-lg md:text-base sm:text-sm mb-8 leading-relaxed">
+          <p
+            className="text-lg md:text-base sm:text-sm mb-8 leading-relaxed"
+            style={{ color: "white" }}
+          >
             The perfect blend of boxing techniques and full-body fitness crafted
             for women. <br />
             Build strength and confidence with every punch!
