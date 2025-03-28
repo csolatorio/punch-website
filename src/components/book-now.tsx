5693f3e51087e5522591eb0d16f346ef132d9b2f
csolatorio <solatoriocarylethel@gmail.com>
2025-03-28 19:19:25 +0800

    updated homepage

--- src/components/book-now.tsx
+++ src/components/book-now.tsx
@@ -8,9 +8,12 @@ const BookBoxingClass: React.FC = () => {
     email: "",
     date: "",
     time: "",
+    classType: "Personal Training",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
   };
@@ -18,10 +21,10 @@ const BookBoxingClass: React.FC = () => {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const { name, email, date, time } = form;
-    const message = `🥊 Boxing Class Booking:\n\n👤 Name: ${name}\n📧 Email: ${email}\n📅 Date: ${date}\n⏰ Time: ${time}`;
+    const { name, email, date, time, classType } = form;
+    const message = `Boxing Class Booking:\n\n👤 Name: ${name}\n📧 Email: ${email}\n📅 Date: ${date}\n⏰ Time: ${time}\n📌 Class Type: ${classType}`;
 
-    const phoneNumber = "85294338092"; // WhatsApp number with country code
+    const phoneNumber = "85294338092";
     const encodedMessage = encodeURIComponent(message);
     const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 
@@ -32,9 +35,7 @@ const BookBoxingClass: React.FC = () => {
     <div className="relative w-full h-screen text-white">
       <Navbar />
       <div className="max-w-xl mx-auto mb-20 p-6 bg-[#EBE8DE] shadow-xl rounded-2xl mt-10">
-        <h2 className="text-3xl font-bold mb-4 text-center">
-          Book a Boxing Class
-        </h2>
+        <h2 className="text-3xl font-bold mb-4 text-center">Book a Class</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="space-y-2">
             <label className="block mb-1 font-medium text-black">Name</label>
@@ -86,6 +87,23 @@ const BookBoxingClass: React.FC = () => {
               className="text-black w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
+          </div>
+
+          <div>
+            <label className="block mb-1 font-medium text-black">
+              Class Type
+            </label>
+            <select
+              name="classType"
+              value={form.classType}
+              onChange={handleChange}
+              className="text-black w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            >
+              <option value="Personal Training">Personal Training</option>
+              <option value="Small Group">Small Group</option>
+              <option value="Corporate Classes">Corporate Classes</option>
+            </select>
           </div>
 
           <button
